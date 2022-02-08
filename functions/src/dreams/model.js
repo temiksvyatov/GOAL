ce9a0441b4e1539346261c4f34857800f0c8eb04
yoloroy <slart2108@gmail.com>
2022-02-08 20:29:39 +0300

    :memo::recycle: start of refactor of onCall functions + add documentation

---
+++ functions/src/dreams/model.js
@@ -0,0 +1,27 @@
+/**
+ * Challenge
+ * @typedef {Object} Challenge
+ * @property {string} id
+ * @property {number} no № in dream's dreams list
+ * @property {string} name
+ * @property {string} description
+ */
+
+/**
+ * Detailed Dream
+ * @typedef {Object} DetailedDream
+ * @property {string} id
+ * @property {string} name
+ * @property {string} description
+ * @property {string} tags
+ * @property {Array<Challenge>} challenges
+ */
+
+/**
+ * just Dream
+ * @typedef {Object} Dream
+ * @property {string} id
+ * @property {string} name
+ * @property {string} description
+ * @property {Array<string>} tags
+ */
